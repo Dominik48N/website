74cd9ed593930e39d2b5c59733f3ed809a421598
Dominik <admin@dominik48n.de>
2021-05-12 15:21:10 +0200

    Remove the default app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.sass';
 
 import { Information } from "./components/information";
@@ -9,18 +8,6 @@ export function App() {
     <div className="App">
       <header className="App-header">
         <Information />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
