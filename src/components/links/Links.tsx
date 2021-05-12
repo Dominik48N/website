53aed10a0439f30a67ed3fb82ad86a5eb435fcb7
Dominik <admin@dominik48n.de>
2021-05-12 20:51:46 +0200

    Add the links

--- src/components/links/Links.tsx
+++ src/components/links/Links.tsx
@@ -6,8 +6,10 @@ export class Links extends React.Component {
 
     render() {
         return (
-            <div>
-
+            <div className="container">
+                <a href="/projects" className="left-text link">Projects</a>
+                <a href="/skills" className="left-text link">Skills</a>
+                <a href="/contact" className="link">Contact</a>
             </div>
         );
     }
