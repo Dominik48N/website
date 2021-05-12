1b6f99c4b0a768df4485c466ec4e8fbb89f8946e
Dominik <admin@dominik48n.de>
2021-05-12 15:28:22 +0200

    Create links component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.sass';
 
 import { Information } from "./components/information";
+import { Links } from "./components/links";
 
 export function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Information />
+          <Information />
+          <Links />
       </header>
     </div>
   );
---
+++ src/components/links/Links.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Links.sass";
+
+export class Links extends React.Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+
+}
