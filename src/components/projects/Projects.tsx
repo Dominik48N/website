f82a3264e2b3853e794ac2bea3c1c9b1dc3c1bb7
Dominik <admin@dominik48n.de>
2021-05-17 11:02:19 +0200

    Create projects component

---
+++ src/components/projects/Projects.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export class Projects extends React.Component {
+
+    render() {
+        return (
+            <header className="App-header">
+                <h3>Projects</h3>
+
+                <div className="projects-container">
+
+                </div>
+            </header>
+        );
+    }
+
+}
+
