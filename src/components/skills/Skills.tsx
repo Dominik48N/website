4f41697bb46c9a488c845e96003c2de621202e6e
Dominik <admin@dominik48n.de>
2021-05-17 10:55:00 +0200

    Add a skills title

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -9,6 +9,8 @@ export class Skills extends React.Component {
     render() {
         return (
             <header className="App-header">
+                <h3>Skills</h3>
+
                 <div className="skills-container">
                     <Languages />
                     <Databases />
